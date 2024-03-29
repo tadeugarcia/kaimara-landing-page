be04c6368a0bbb4fd3c6f4f0b5bd5cae48957141
Tadeu <tadeugarcia.92@gmail.com>
2024-03-29 17:15:31 -0300

    repor consts

--- admin/medusa-config.js
+++ admin/medusa-config.js
@@ -21,6 +21,18 @@ try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
 } catch (e) {}
 
+// CORS when consuming Medusa from admin
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+
+// CORS to avoid issues when consuming Medusa from a client
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
