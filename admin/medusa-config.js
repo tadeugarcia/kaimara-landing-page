2c0f09dd2b36dcccef670d8d619e734f841d03df
Tadeu Garcia <tadeugarcia.92@gmail.com>
2024-07-08 21:09:00 -0300

    Ajustes de deploy seguindo a documentação do medusajs

--- admin/medusa-config.js
+++ admin/medusa-config.js
@@ -23,15 +23,15 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://viveiroprimavera.kaimara.com.br";
+  process.env.ADMIN_CORS || "https://viveiroprimavera.kaimara.com.br";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://viveiroprimavera.kaimara.com.br";
+const STORE_CORS = process.env.STORE_CORS || "https://viveiroprimavera.kaimara.com.br";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://postgres.cyaldmgcviavijxoulwf:vmoBY847F0GW8bf4@aws-0-sa-east-1.pooler.supabase.com:5432/postgres";
 
-const REDIS_URL = process.env.REDIS_URL || "redis://viveiroprimavera.kaimara.com.br";
+const REDIS_URL = process.env.REDIS_URL || "redis://default:AdT_AAIncDEyOTllMDA1YjZhNjU0MjA2ODhmYmIwMzgwZDViYTkwYXAxNTQ1Mjc@equal-drake-54527.upstash.io:6379";
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -82,7 +82,15 @@ const projectConfig = {
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
 module.exports = {
-  projectConfig,
+  projectConfig: {
+    // ...
+    database_extra: process.env.NODE_ENV !== "development" ?
+      {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      } : {},
+  },
   plugins,
   modules,
   featureFlags: {
