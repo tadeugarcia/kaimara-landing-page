ae719160240d9f30b61014a09dca3892aa2a959b
Tadeu Garcia <tadeugarcia.92@gmail.com>
2024-07-09 23:08:25 -0300

    Removendo arquivos desnecessários

--- node_modules/@medusajs/medusa/dist/api/routes/admin/price-lists/modules-queries/get-price-list.d.ts
+++
@@ -1,5 +0,0 @@
-import { MedusaContainer } from "@medusajs/types";
-import { PriceList } from "../../../../../models";
-export declare function getPriceListPricingModule(id: string, { container, }: {
-    container: MedusaContainer;
-}): Promise<PriceList>;
